c8d5911fbd380439c2f62cb7e5d162bcb4deca92
KlemensasKaubrys <kaubrys.klemensas@outlook.com>
2024-08-07 17:52:45 +0300

    Replaced alacritty with wezterm

--- dwm/config.h
+++ dwm/config.h
@@ -97,7 +97,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-bw", "2", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "wezterm", NULL };
 static const char *pvc[]  = { "pavucontrol", NULL };
 static const char *fm[] = { "Thunar", NULL};
 static const char *browser[] = { "firefox", NULL};
