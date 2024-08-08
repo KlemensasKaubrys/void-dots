79bfa1e46ad35abc5eaa7a63dc3cb3db9cb26f33
Klemensas Kaubrys <kaubrys.klemensas@outlook.com>
2024-08-09 01:36:26 +0300

    Enabled mpd service

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ chown -R $username:$username /home/$username/.config/gtk-3.0
 xbps-install -S base-devel xorg libX11-devel libXft-devel libXinerama-devel freetype-devel fontconfig-devel elogind xinit xprop picom dunst feh git btop Thunar network-manager-applet mate-polkit pipewire wireplumber picard mpd ncmpcpp yt-dlp flameshot lutris gvfs lxappearance pavucontrol arc-theme nerd-fonts font-awesome6 font-inter mesa-dri xf86-video-amdgpu mesa-vaapi mesa-vdpau vulkan-loader mesa-vulkan-radeon amdvlk firefox betterlockscreen libspa-bluetooth bluez anki syncthing wezterm mpv fastfetch curl 7zip xauth starship ffmpeg ufw 
 
 ln -s /etc/sv/dbus /var/service/
+ln -s /etc/sv/mpd /var/service/
 ln -s /etc/sv/bluetoothd /var/service/
 
 # Disabling bitmaps because they break firefox fonts
